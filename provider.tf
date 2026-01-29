51ed5c65c66129230bc32ad956c08c0f5448af71
Indyalamounika <vedamouni01@gmail.com>
2026-01-29 17:48:23 +0530

    jenkins

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-mounika"
+    bucket = "remotestate-mounika"
     key    = "roboshop-dev-jenkins"
     region = "us-east-1"
     use_lockfile = true
